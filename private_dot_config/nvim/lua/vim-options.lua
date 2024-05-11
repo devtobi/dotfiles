68142b41093a6694e2c334b28bf168a88c43044c
devtobi <28538704+devtobi@users.noreply.github.com>
2024-05-11 13:45:04 +0200

    Added tmux <-> nvim integration

--- private_dot_config/nvim/lua/vim-options.lua
+++ private_dot_config/nvim/lua/vim-options.lua
@@ -2,5 +2,11 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
+
+vim.wo.number = true
+
 vim.g.mapleader = " "
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
