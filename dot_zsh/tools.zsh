c59f33972d6f4febf3f5dc26f26ebf9975783098
devtobi <28538704+devtobi@users.noreply.github.com>
2024-08-15 12:53:53 +0200

    Added bun autocompletions

--- dot_zsh/tools.zsh
+++ dot_zsh/tools.zsh
@@ -4,3 +4,5 @@ source $(brew --prefix)/bin/virtualenvwrapper.sh
 # Misc CLI tools
 eval $(thefuck --alias)
 eval "$(zoxide init zsh)"
+# bun completions
+[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
