ee3302cf34064545baf3dc1fd3ec3f423ed61731
devtobi <28538704+devtobi@users.noreply.github.com>
2025-04-11 17:52:28 +0200

    Removed unused env variables

--- dot_zsh/env.zsh
+++ dot_zsh/env.zsh
@@ -1,8 +1,4 @@
 export XDG_CONFIG_HOME=$HOME/.config
 export SSH_AUTH_SOCK=$HOME/.ssh/agent
-export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=$(brew --prefix)/bin/python3
-export VIRTUALENVWRAPPER_VIRTUALENV=$(brew --prefix)/bin/virtualenv
 export PNPM_HOME=$HOME/.pnpm
 export NI_CONFIG_FILE=$XDG_CONFIG_HOME/ni/nirc
-export PYENV_ROOT=$HOME/.pyenv
