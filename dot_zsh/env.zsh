f58e27dc08040770236c7300c82432cb814a9444
devtobi <28538704+devtobi@users.noreply.github.com>
2025-05-03 15:12:31 +0200

    Disabled COREPACK setting of packageManager field

--- dot_zsh/env.zsh
+++ dot_zsh/env.zsh
@@ -1,4 +1,5 @@
 export XDG_CONFIG_HOME=$HOME/.config
 export SSH_AUTH_SOCK=$HOME/.ssh/agent
 export PNPM_HOME=$HOME/.pnpm
+export COREPACK_ENABLE_AUTO_PIN=0 # disable auto pin to not set packageManager field in package.json files
 export NI_CONFIG_FILE=$XDG_CONFIG_HOME/ni/nirc
