fb5c0e1a71b2a2f12b63dbf13990a39a945d66c7
devtobi <28538704+devtobi@users.noreply.github.com>
2025-12-25 17:19:25 +0100

    Fixed terminal hanging due to nofification error

--- dot_zsh/plugins.zsh
+++ dot_zsh/plugins.zsh
@@ -1,5 +1,5 @@
 autoload -Uz compinit
 compinit
 zstyle ':antidote:bundle' use-friendly-names 'yes'
-source /$(brew --prefix antidote)/share/antidote/antidote.zsh
+source $(brew --prefix antidote)/share/antidote/antidote.zsh
 antidote load
